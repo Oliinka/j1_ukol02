22abc2febe485ed5db4ea0141c2f2a48ec1708c9
Olinka <olga.titzenthaler@gmail.com>
2024-02-05 01:17:49 +0100

    SNOWMAN, ICECREAM, TRAIN DONE, CTRL+ALT+L

--- src/main/java/cz/czechitas/turtle/HlavniProgram.java
+++ src/main/java/cz/czechitas/turtle/HlavniProgram.java
@@ -13,37 +13,42 @@ public class HlavniProgram {
 
     public void start() {
         //TODO Tady bude kód pro kreslení želví grafiky.
-    zofka.setX(200);
-    zofka.setY(100);
 
-    //ICECREAM
-    zofka.turnLeft(180);
-    drawTriangle(150,80, Color.CYAN);
-    zofka.turnRight(90);
-    zofka.move(80);
-    zofka.turnRight(90);
-    drawCircle(40, Color.PINK);
 
-    zofka.setX(350);
-    zofka.setY(50);
+        //MOVE TO SPECIFIC COORDINATES
+        zofka.setX(200);
+        zofka.setY(100);
 
-    //SNOWMAN
-    drawSnowmanBody(40, Color.BLUE);
-    zofka.move(40+20+(40+40)*2);
-    zofka.turnLeft(90);
-    zofka.move(40+20+20+20);
-    zofka.turnRight(90);
-    drawCircle(20,Color.BLUE);
-    zofka.turnRight(90);
-    zofka.move(20*2+(40+20)*2);
-    zofka.turnLeft(90);
-    drawCircle(20, Color.BLUE);
+        //ICECREAM
+        zofka.turnLeft(180);
+        drawTriangle(150, 80, Color.CYAN);
+        zofka.turnRight(90);
+        zofka.move(80);
+        zofka.turnRight(90);
+        drawCircle(40, Color.PINK);
 
-    zofka.setX(600);
-    zofka.setY(300);
+        //MOVE TO SPECIFIC COORDINATES
+        zofka.setX(350);
+        zofka.setY(50);
 
-    //TRAIN
-    drawTrain();
+        //SNOWMAN
+        drawSnowmanBody(40, Color.BLUE);
+        zofka.move(40 + 20 + (40 + 40) * 2);
+        zofka.turnLeft(90);
+        zofka.move(40 + 20 + 20 + 20);
+        zofka.turnRight(90);
+        drawCircle(20, Color.BLUE);
+        zofka.turnRight(90);
+        zofka.move(20 * 2 + (40 + 20) * 2);
+        zofka.turnLeft(90);
+        drawCircle(20, Color.BLUE);
+
+        //MOVE TO SPECIFIC COORDINATES
+        zofka.setX(600);
+        zofka.setY(300);
+
+        //TRAIN
+        drawTrain();
 
     }
 
@@ -60,7 +65,7 @@ public class HlavniProgram {
         zofka.penUp();
     }
 
-    //FULLY AWARE THAT THIS METHOD IS UNUSED, JUST THINK i MIGHT NEED IT FOR THE NEXT HOMEWORKS
+    //FULLY AWARE THAT THIS METHOD IS UNUSED, JUST THINK I MIGHT NEED IT FOR THE NEXT HOMEWORKS
     public void drawEquilateralTriangle(float sideA, Color color) {
         zofka.setPenColor(color);
         zofka.penDown();
@@ -77,70 +82,71 @@ public class HlavniProgram {
         zofka.penDown();
         zofka.turnRight(90);
         zofka.move(base);
-        double angle = Math.atan2(base/2, height);
-        zofka.turnLeft(90+Math.toDegrees(angle));
+        double angle = Math.atan2(base / 2, height);
+        zofka.turnLeft(90 + Math.toDegrees(angle));
         zofka.move(Math.hypot(base / 2, height));
-        zofka.turnLeft(180-2*Math.toDegrees(angle));
+        zofka.turnLeft(180 - 2 * Math.toDegrees(angle));
         zofka.move(Math.hypot(base / 2, height));
         zofka.penUp();
-        zofka.turnRight(180-Math.toDegrees(angle));
+        zofka.turnRight(180 - Math.toDegrees(angle));
     }
 
 
     public void drawCircle(int pericular, Color color) {
-            zofka.penDown();
-            for (int i = 0; i < 360; i++) {
-                zofka.setPenColor(color);
-                zofka.move(2 * Math.PI * pericular / 360);
-                zofka.turnRight(1);
-            }
-            zofka.penUp();}
+        zofka.penDown();
+        for (int i = 0; i < 360; i++) {
+            zofka.setPenColor(color);
+            zofka.move(2 * Math.PI * pericular / 360);
+            zofka.turnRight(1);
+        }
+        zofka.penUp();
+    }
 
     public void drawSnowmanBody(int pericular, Color color) {
         zofka.penUp();
         for (int i = 0; i < 3; i++) {
             zofka.turnRight(180);
-            zofka.move(pericular + i*20);
+            zofka.move(pericular + i * 20);
             zofka.turnRight(90);
-            zofka.move(pericular + i*20);
+            zofka.move(pericular + i * 20);
             zofka.turnRight(90);
             zofka.penDown();
-            drawCircle(pericular + i*20, color);
+            drawCircle(pericular + i * 20, color);
             zofka.penUp();
             zofka.turnRight(90);
-            zofka.move(pericular + i*20);
+            zofka.move(pericular + i * 20);
             zofka.turnRight(90);
-            zofka.move(pericular + i*20);
+            zofka.move(pericular + i * 20);
             zofka.turnRight(180);
         }
     }
 
     //TRAIN
-     public void drawTrain() {
-            drawRectangle(120,250, Color.MAGENTA);
-            zofka.turnRight(90);
-            zofka.move(250);
-            zofka.turnLeft(90);
-            drawRectangle(200,160, Color.BLACK);
-            drawCircle(80,Color.BLACK);
-            zofka.turnLeft(90);
-            zofka.move(90);
-            zofka.turnLeft(180);
-            drawCircle(40, Color.BLACK);
-            zofka.turnLeft(180);
-            zofka.move(40+30);
-            zofka.turnLeft(90);
-            zofka.move(40);
-            drawCircle(40, Color.BLACK);
-            zofka.turnRight(90);
-            zofka.move(90);
-            zofka.turnRight(90);
-            zofka.move(100);
-            zofka.turnLeft(45);
-            zofka.turnLeft(180);
-            drawTriangle(80,160, Color.MAGENTA);
-            zofka.turnRight(45);
-            zofka.turnRight(180);
-        }
+    public void drawTrain() {
+        drawRectangle(120, 250, Color.MAGENTA);
+        zofka.turnRight(90);
+        zofka.move(250);
+        zofka.turnLeft(90);
+        drawRectangle(200, 160, Color.BLACK);
+        drawCircle(80, Color.BLACK);
+        zofka.turnLeft(90);
+        zofka.move(90);
+        zofka.turnLeft(180);
+        drawCircle(40, Color.BLACK);
+        zofka.turnLeft(180);
+        zofka.move(40 + 30);
+        zofka.turnLeft(90);
+        zofka.move(40);
+        drawCircle(40, Color.BLACK);
+        zofka.turnRight(90);
+        zofka.move(90);
+        zofka.turnRight(90);
+        zofka.move(100);
+        zofka.turnLeft(45);
+        zofka.turnLeft(180);
+        drawTriangle(80, 160, Color.MAGENTA);
+        zofka.turnRight(45);
+        zofka.turnRight(180);
     }
+}
 
